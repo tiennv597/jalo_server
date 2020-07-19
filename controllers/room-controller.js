29d9aa5fe9b51d43a54f7610881ff09925c5ad83
tiennv <tiennv597@gmail.com>
2020-07-19 21:38:46 +0700

    coding connect socket

--- controllers/room-controller.js
+++ controllers/room-controller.js
@@ -12,24 +12,6 @@ module.exports = function (io, socket, namespace, listRoom) {
     var d = new Date();
     var r = Math.floor((Math.random() * 10));
     var id_room = d.getMilliseconds().toString() + r.toString();
-    // var roomObj = {
-    //   'id_room': id_room,
-    //   "id_owner": userId,
-    //   "name_noom": "",
-    //   "password": "",
-    //   'level': level,
-    //   "quantity": quantity,
-    //   "time": time,
-    //   "type": type,
-    //   "users": [
-    //     {
-    //       "id": userId,
-    //       "fullName": fullName,
-    //       "gender": "",
-    //       "profileUrl": ""
-    //     }
-    //   ]
-    // }
     var info = {
       'id_room': id_room,
       "id_owner": userId,
@@ -55,10 +37,11 @@ module.exports = function (io, socket, namespace, listRoom) {
     roomObj.users.set(userId, user);
 
     listRoom.set(id_room, roomObj);
+    let users = Array.from(roomObj.users.values());
     socket.join(id_room);
     var r = {
       'info': info,
-      'users': user,
+      'all_user': users,
     }
     var room = JSON.stringify(r);
     console.log(room);
@@ -76,18 +59,24 @@ module.exports = function (io, socket, namespace, listRoom) {
       "profileUrl": ""
     }
     roomObj.users.set(userId, user);
-    console.log(roomObj);
-    var room = JSON.stringify(roomObj);
+    //let u = roomObj.users.values();
+    let users = Array.from(roomObj.users.values());
+    var r = {
+      'info': roomObj.info,
+      'all_user': users,
+    }
+    var room = JSON.stringify(r);
     console.log(room);
+
     socket.join(id_room);
     nsp.to(id_room).emit(SOCKET_CONSTANT.joined_room, room);
 
   });
 
   socket.on(SOCKET_CONSTANT.leave, (id_room) => {
-    let obj_room = { id_room };
+    //let obj_room = { id_room };
     socket.leave(id_room);
-    listRoom.delete(obj_room);
+    listRoom.delete(id_room);
     nsp.to(id_room).emit(SOCKET_CONSTANT.leave);
   });
 
